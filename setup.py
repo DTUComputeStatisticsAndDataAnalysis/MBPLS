f1e4d867a6bffb4a33562bcf8550d0dc994339e2
Laurent Vermue <lvermue@users.noreply.github.com>
2018-10-15 13:02:21 +0200

    Reformat jupyter notebooks and update setup file 	modified:   examples/CarbohydrateMicroarrays/Carbohydrate_Microarray_PLS.ipynb 	modified:   examples/FTIR/FTIR_PLS.ipynb 	modified:   examples/MBPLS_intro/MBPLS_introduction.ipynb 	modified:   examples/MBPLS_intro/MultiblockPLS_PectinYield.ipynb 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -13,8 +13,12 @@ from setuptools import setup, find_packages
 import mbpls
 
 NAME = "mbpls"
-DESCRIPTION = "An implementation of the most common partial least squares algorithm as multi-block methods"
 VERSION = mbpls.__version__
+DESCRIPTION = "An implementation of the most common partial least squares algorithm as multi-block methods"
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+    LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown',
+URL = 'https://github.com/b0nsaii/MBPLS'
 AUTHORS = "Andreas Baum, Laurent Vermue"
 AUTHOR_MAILS = "<andba@dtu.dk>, <lauve@dtu.dk>"
 LICENSE = 'new BSD'
@@ -28,6 +32,9 @@ SCIKIT_LEARN_MIN_VERSION = '0.18.0'
 setup(name=NAME,
       version=VERSION,
       description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+      url=URL,
       author=AUTHORS,
       author_email=AUTHOR_MAILS,
       packages = find_packages(),
@@ -45,12 +52,13 @@ setup(name=NAME,
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6',
                    'Programming Language :: Python :: 3.7',
-                   'Development Status :: 2 - Pre-Alpha'
+                   'Development Status :: 3 - Alpha'
                    ],
       install_requires=[
           'numpy>={0}'.format(NUMPY_MIN_VERSION),
           'matplotlib>={0}'.format(MATPLOTLIB_MIN_VERSION),
           'scipy>={0}'.format(SCIPY_MIN_VERSION),
           'scikit-learn>={0}'.format(SCIKIT_LEARN_MIN_VERSION)
-            ]
+            ],
+      python_requires='>=3.5',
       )
