e1fd46cf1d75fe942531feadecfa69d188232bae
Laurent Vermue <lvermue@users.noreply.github.com>
2018-11-14 11:52:56 +0100

    Update version info and add Travis CI test instructions for Windows
    	modified:   .travis.yml
    	modified:   mbpls/__init__.py
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ def setup_package():
                        'Programming Language :: Python :: 3.5',
                        'Programming Language :: Python :: 3.6',
                        'Programming Language :: Python :: 3.7',
-                       'Development Status :: 3 - Alpha'
+                       'Development Status :: 5 - Production/Stable'
                        ],
           install_requires=[
               'numpy>={0}'.format(NUMPY_MIN_VERSION),
